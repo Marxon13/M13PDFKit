0b89d818e54c9524373f205bbc88c2ecd0ee18fc
Marxon13 <brandon.mcquilkin@gmail.com>
2014-11-20 19:18:22 -0500

    Added the scroll view for debugging

---
+++ M13PDFKit/DebugScrollView.m
@@ -0,0 +1,42 @@
+//
+//  DebugScrollView.m
+//  M13PDFKit
+//
+//  Created by Brandon McQuilkin on 5/14/14.
+//  Copyright (c) 2014 Brandon McQuilkin. All rights reserved.
+//
+
+#import "DebugScrollView.h"
+
+@implementation DebugScrollView
+
+- (void)setContentOffset:(CGPoint)contentOffset
+{
+    NSLog(@"Set Content Override: %@", NSStringFromCGPoint(contentOffset));
+    
+    if (contentOffset.x == 0 && contentOffset.y == 0) {
+        NSLog(@"Break");
+    }
+    
+    [super setContentOffset:contentOffset];
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    NSLog(@"Set Frame Override: %@", NSStringFromCGRect(frame));
+    [super setFrame:frame];
+}
+
+- (void)setBounds:(CGRect)bounds
+{
+    NSLog(@"Set Bounds Override: %@", NSStringFromCGRect(bounds));
+    [super setBounds:bounds];
+}
+
+- (void)setContentSize:(CGSize)contentSize
+{
+    NSLog(@"Set Content Size Override: %@", NSStringFromCGSize(contentSize));
+    [super setContentSize:contentSize];
+}
+
+@end
